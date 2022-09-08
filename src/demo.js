a4b19db0f8560efc7b1afcd7cc7c20d6beebf1f5
putn1k <skachok11@yandex.ru>
2022-09-08 09:30:59 +0300

    добавлена верстка в исходники

---
+++ src/demo.js
@@ -0,0 +1,13 @@
+const accordion = new justAccordion( '.accordion', {
+  speed: 100,
+  showFirst: true,
+  showOnlyOne: true,
+  activeHandlerClass: 'custom-active-class-btn',
+  activeContentClass: 'custom-active-class-content',
+  isOpen: ( acc ) => {
+    console.log( acc );
+  },
+  isClose: ( acc ) => {
+    console.log( acc );
+  }
+} );
